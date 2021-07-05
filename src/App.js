82ebd4f3179b8ce39ddf79e663781faf0eb5b979
Pegah1368 <79860502+Pegah1368@users.noreply.github.com>
2021-07-05 12:03:09 +0200

    Added Github repo Url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://fervent-albattani-67c43b.netlify.app/"
+          target="blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open source
+        </a>
+        Coded by Pegah Bozorgmanesh
+      </footer>
     </div>
   );
 }
