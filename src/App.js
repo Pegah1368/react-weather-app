3e35f5db2aad5ed033de4c5c7cb591bbc9291d84
Pegah1368 <79860502+Pegah1368@users.noreply.github.com>
2021-07-05 16:06:46 +0200

    Added HTML

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Weather from "./Weather";
-import "./Weather.css";
+
 export default function App() {
   return (
     <div className="App">
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,44 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1>Stockholm</h1>
+      <ul>
+        <li>Monday 15:00</li>
+        <li>Partly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="partly cloudy"
+          />
+          26º C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation:70%</li>
+            <li>Humidity:45%</li>
+            <li>Wind: 7 m/s</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
