354b656b02bce92d448011e7cbd92e745630af4b
Pegah1368 <79860502+Pegah1368@users.noreply.github.com>
2021-07-05 15:27:42 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
-
+import Weather from "./Weather";
+import "./Weather.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://fervent-albattani-67c43b.netlify.app/"
-          target="blank"
-          rel="noreferrer"
-        >
-          Open source{" "}
-        </a>
-        Coded by Pegah Bozorgmanesh
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          <a
+            href="https://fervent-albattani-67c43b.netlify.app/"
+            target="blank"
+            rel="noreferrer"
+          >
+            Open source{" "}
+          </a>
+          Coded by Pegah Bozorgmanesh
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="weather">
+      <h1>Stockholm</h1>
+    </div>
+  );
+}
